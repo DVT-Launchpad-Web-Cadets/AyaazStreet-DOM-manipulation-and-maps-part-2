a7b7f9e42ec2207b127f45fbcafb661a179e8f81
Ayaaz Street <astreet@dvtsoftware.com>
2024-02-21 16:52:49 +0200

    perf: :zap: Changed how point is on map
    Made a single point that moves on map instead of creating and deleting multiple points

--- go-kart-tracker/map.js
+++ go-kart-tracker/map.js
@@ -2,8 +2,13 @@ import "./style.scss";
 import { getLap } from "./api-calls";
 
 const filename = "SN2780_210722_11H00_NADINE_IDUBE_RACEWAY_16_5554.json";
-let map = L.map("map");
-let points = L.layerGroup();
+let map = L.map("map", { zoomControl: false });
+let point = L.circle([0, 0], {
+  color: "rgb(211, 82, 100)",
+  fillColor: "rgb(211, 82, 100)",
+  fillOpacity: 1,
+  radius: 3,
+});
 let polylines = L.layerGroup();
 let latLngs = [];
 let drawing = false;
@@ -14,22 +19,18 @@ export default function getMapData() {
 
 export function drawLap(lapNumber) {
   drawing = true;
+  point.removeFrom(map);
   for (let index = 0; index < latLngs.length; index++) {
     polylines.removeFrom(map);
-    points.removeFrom(map);
   }
-  points.clearLayers();
   polylines.clearLayers();
   latLngs = [];
   getLap(filename, lapNumber, draw);
 }
 
 const showMapData = (data) => {
-  const jsonLatitude = "Lat\u{002E}";
-  const jsonLongitude = "Lon\u{002E}";
-
-  const latitude = data.dataSet[0][jsonLatitude] * Math.pow(10, -6);
-  const longitude = data.dataSet[0][jsonLongitude] * Math.pow(10, -6);
+  const latitude = data.dataSet[0]["Lat."] * Math.pow(10, -6);
+  const longitude = data.dataSet[0]["Lon."] * Math.pow(10, -6);
   map.setView([latitude, longitude], 17);
 
   L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
@@ -37,17 +38,6 @@ const showMapData = (data) => {
     maxZoom: 19,
   }).addTo(map);
 };
-
-function drawPoint(latitude, longitude) {
-  let point = L.circle([latitude, longitude], {
-    color: "rgb(211, 82, 100)",
-    fillColor: "rgb(211, 82, 100)",
-    fillOpacity: 1,
-    radius: 3,
-  });
-  points.addLayer(point);
-  points.addTo(map);
-}
 
 function drawPolyline() {
   let polyline = L.polyline(latLngs, {
@@ -65,22 +55,18 @@ function drawPolyline() {
 
 const draw = (data) => {
   drawing = false;
+  point.addTo(map);
   let index = 0;
   let myTimeout = setInterval(() => {
-    if (drawing)
-      clearInterval(myTimeout);
+    if (drawing) clearInterval(myTimeout);
     let speed = data.dataSet[index]["Speed GPS"];
-    const jsonLatitude = "Lat\u{002E}";
-    const jsonLongitude = "Lon\u{002E}";
-    let latitude = data.dataSet[index][jsonLatitude] * Math.pow(10, -6);
-    let longitude = data.dataSet[index][jsonLongitude] * Math.pow(10, -6);
-
-    points.clearLayers();
+    let latitude = data.dataSet[index]["Lat."] * Math.pow(10, -6);
+    let longitude = data.dataSet[index]["Lon."] * Math.pow(10, -6);
     polylines.clearLayers();
     latLngs.push([latitude, longitude]);
     drawPolyline();
-    drawPoint(latitude, longitude);
+    point.setLatLng([latitude, longitude]);
 
     index < data.dataSet.length - 1 ? index++ : clearInterval(myTimeout);
-  }, data.dataSet[index]["Speed GPS"] / 20);
+  }, data.dataSet[index]["Speed GPS"] / 10);
 };
